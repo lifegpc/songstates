d816bcaf6176f9e791868b3d1a06de155cc8f7d1
lifegpc <g1710431395@gmail.com>
2020-01-20 10:54:09 +0800

    update analysis

--- analysis.py
+++ analysis.py
@@ -1,13 +1,25 @@
 import sys
 import os
+import XMLParser
+import json
+import time
 h="""useage:
 \tanalysis.py [-h] [-a] inputfile outputfile
 choice:
 \t-h\t显示帮助
 \t-a\t分析所有时间的数据
+inputfile:支持XML和JSON文件
+outputfile:输出文件夹名称
 """
 def main(filen:str,filen2:str,settings:dict) :
-    pass
+    try :
+        re=XMLParser.loadXML(filen)
+    except:
+        f=open(filen2,'r',encoding='utf8')
+        re=json.load(f)
+        f.close()
+    if os.path.exists(filen2) :
+        removedir(filen2)
 def getchoice(settings:dict,i:str):
     "解析是否为选项，不是选项返回0，是选项但解析失败返回1"
     if len(i)>=1 and i[0]=='-':
@@ -18,6 +30,21 @@ def getchoice(settings:dict,i:str):
             return 1
     else:
         return 0
+def removedir(f:str) :
+    "删除文件夹"
+    fl=os.listdir(f)
+    for i in fl :
+        i='%s\\%s'%(f,i)
+        if os.path.isdir(i) :
+            removedir(i)
+        else:
+            print(i)
+            os.remove(i)
+    print(f)
+    try :
+        os.removedirs(f)
+    except :
+        pass
 if __name__=="__main__" :
     if len(sys.argv)>1 :
         name=""
@@ -37,8 +64,12 @@ if __name__=="__main__" :
                     print('"%s"文件不存在或是一个文件夹'%(i))
                     exit(-1)
             elif m==1 and read==0 :
-                name2=i
-                m=m+1
+                if not os.path.exists(i) or (os.path.exists(i) and os.path.isdir(i)) :
+                    name2=i
+                    m=m+1
+                else :
+                    print('"%s"文件已存在'%(i))
+                    exit(-1)
         if m==2 :
             main(name,name2,settings)
         else :
